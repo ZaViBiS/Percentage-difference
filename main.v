2bc5dd164393fa2f8ec8df9236e35ea9b19b2176
ZaViBiS <igordnepr27@gmail.com>
2021-12-29 19:10:18 +0200

    init

---
+++ main.v
@@ -0,0 +1,19 @@
+import os
+
+import nedpals.vargs
+
+fn main() {
+	mut parameter := vargs.new(os.args, 0)
+	parameter.parse()
+	first := parameter.unknown[0].f64()
+	second := parameter.unknown[1].f64()
+	if first > second {
+		println('${cal(first, second)} %')
+	}else {
+		println('${cal(second, first)} %')
+	}
+}
+
+fn cal(first f64, second f64) f64 {
+	return (first/second-1)*100
+}
